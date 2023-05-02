dc571a1d9e03cb60b6ef7b80f293cec0caf518f5
Matt Kwiecien <mattkwiecien@gmail.com>
2023-05-01 19:54:09 -0700

    Updating helper script for running calibration on perlmutter.

--- bin/redmapper-shifter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Load the redmapper container using shifter
-SHIFTER=docker:ghcr.io/erykoff/redmapper:0.8.5.5
-echo 'Updating and loading the shifter image '$SHIFTER
-shifterimg pull $SHIFTER
-shifter --module=mpich --image $SHIFTER bash
